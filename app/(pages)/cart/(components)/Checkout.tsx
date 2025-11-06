1042291b84cbe2f60500c23b9cb55f2a6f816537
Giuseppe <cresctelli@gmail.com>
2025-11-06 14:22:51 +0100

    Added order info in cart summary

--- app/(pages)/cart/(components)/Checkout.tsx
+++ app/(pages)/cart/(components)/Checkout.tsx
@@ -5,16 +5,40 @@ import { createOrder } from "@/app/(lib)/actions";
 
 function Checkout() {
   const { cart, clearCart } = useCart();
+  const total = cart.reduce(
+    (acc, product) => acc + product.quantity * product.price,
+    0
+  );
 
   return (
     <div className="bg-gray-800 p-4 row-span-2 rounded-md flex flex-col">
+      <h4 className="text-center text-2xl mb-4 border-b-2 px-3 self-center border-emerald-500">
+        Your Cart
+      </h4>
+      {cart.map((p) => (
+        <div
+          key={p.id}
+          className="grid items-center gap-4 grid-cols-[auto_1fr_auto]"
+        >
+          <span className="bg-emerald-500 text-gray-800 font-bold size-7 text-lg text-center rounded-full">
+            {p.quantity}
+          </span>
+          <h3 className="text-xl">{p.name}</h3>
+          <span className="text-xl font-light">
+            ${(p.price * p.quantity).toLocaleString()}
+          </span>
+        </div>
+      ))}
+      <p className="text-center text-xl mt-auto">
+        Total: ${total.toLocaleString()}
+      </p>
       {cart.length > 0 && (
         <Button
           onClick={() => {
             createOrder(cart);
             clearCart();
           }}
-          classes="mt-auto"
+          classes="mt-4"
         >
           Checkout
         </Button>
