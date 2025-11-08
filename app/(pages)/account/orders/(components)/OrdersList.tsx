17bfa33b09f78fa5db6d329ac157eb7b07d36ecb
Giuseppe <cresctelli@gmail.com>
2025-11-08 06:34:12 +0100

    Fixed some Typescript type checking

--- app/(pages)/account/orders/(components)/OrdersList.tsx
+++ app/(pages)/account/orders/(components)/OrdersList.tsx
@@ -3,26 +3,30 @@ import { getCustomer } from "@/app/(services)/apiCustomers";
 import { getOrders } from "@/app/(services)/apiOrders";
 
 async function OrdersList() {
-  const email = (await auth()).user.email;
-  const userId = (await getCustomer(email)).uuid;
+  const user = (await auth())?.user;
+  if (user?.email) {
+    const userId = (await getCustomer(user.email)).uuid;
 
-  const orders = await getOrders(userId);
+    const orders = await getOrders(userId);
 
-  return (
-    <div className="flex flex-col gap-4">
-      {orders.map((ord) => (
-        <div
-          key={ord.id}
-          className="grid grid-cols-2 bg-gray-800 px-4 py-2 rounded-md items-center cursor-pointer"
-        >
-          <p>Created: {new Date(ord.created_at).toLocaleString()}</p>
-          <p>Status: {ord.status}</p>
-          <p>Total: {ord.total}</p>
-          <p>Address: {ord.address}</p>
-        </div>
-      ))}
-    </div>
-  );
+    return (
+      <div className="flex flex-col gap-4">
+        {orders.map((ord) => (
+          <div
+            key={ord.id}
+            className="grid grid-cols-2 bg-gray-800 px-4 py-2 rounded-md items-center cursor-pointer"
+          >
+            <p>Created: {new Date(ord.created_at).toLocaleString()}</p>
+            <p>Status: {ord.status}</p>
+            <p>Total: {ord.total}</p>
+            <p>Address: {ord.address}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return <div>No User</div>;
 }
 
 export { OrdersList };
