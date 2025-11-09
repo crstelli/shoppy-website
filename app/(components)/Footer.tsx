bf1c811111b64f055a13e2f15ba4aabd0b7a7f55
Giuseppe <cresctelli@gmail.com>
2025-11-09 18:10:10 +0100

    Made footer responsive

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,11 +1,13 @@
 function Footer() {
   return (
-    <footer className="flex bg-gray-800 text-gray-500 text-lg justify-center items-center py-8">
-      Made with ❤ by{" "}
-      <a href="https://github.com/crstelli" className="ml-1 underline">
-        crstelli
-      </a>
-      . &copy; 2025 All rights reserved
+    <footer className="flex max-sm:flex-col sm:gap-4 bg-gray-800 text-gray-500 text-lg justify-center items-center py-8">
+      <span>
+        Made with ❤ by{" "}
+        <a href="https://github.com/crstelli" className="ml-1 underline">
+          crstelli
+        </a>
+      </span>{" "}
+      <span>&copy; 2025 All rights reserved</span>
     </footer>
   );
 }
