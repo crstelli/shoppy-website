69d62cd189a9009f2bd6611c00ed1cb78386d30c
Giuseppe <cresctelli@gmail.com>
2025-11-17 18:54:24 +0100

    Fixed responsive logo

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,12 +1,17 @@
 function Footer() {
   return (
-    <footer className="flex max-sm:flex-col sm:gap-4 bg-gray-800 text-gray-500 text-lg justify-center items-center py-8">
+    <footer className="flex max-sm:flex-col bg-gray-800 text-gray-500 text-lg justify-center items-center py-8">
       <span>
-        Made with ❤ by{" "}
-        <a href="https://github.com/crstelli" className="ml-1 underline">
+        Made with ❤ by
+        <a
+          target="blank"
+          href="https://github.com/crstelli"
+          className="ml-1 underline"
+        >
           crstelli
         </a>
-      </span>{" "}
+      </span>
+      <span className="max-sm:hidden px-2">-</span>
       <span>&copy; 2025 All rights reserved</span>
     </footer>
   );
--- app/(components)/TextLogo.tsx
+++ app/(components)/TextLogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function TextLogo() {
   return (
     <h1 className="flex items-center gap-4 text-3xl font-bold text-emerald-500">
-      <Image alt="Company logo" src="logo.svg" width={42} height={42} />
+      <Image alt="Company logo" src="/logo.svg" width={42} height={42} />
       Shoppy
     </h1>
   );
